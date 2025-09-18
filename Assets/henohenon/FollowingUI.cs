6f09e4a15022d7aee9cc0a8ce20e064596b0237f
ramasuke <90756667+ramasuke@users.noreply.github.com>
2025-09-18 12:09:36 +0900

    femlkakfew

---
+++ Assets/henohenon/FollowingUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+public class PlayerFollwingUI : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private void LateUpdate()
+    {
+        transform.position = player.position;
+    }
+
+}
